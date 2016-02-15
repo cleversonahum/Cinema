5e4b18fbaecd3aea8e67139704146955e7dbc401
Antonio Junior <antonio.jr004@gmail.com>
2016-02-15 20:08:34 -0200

    Commit teste
    Versao Teste, Nao to conseguindo pegar o valor da sessao.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Cinema
-Projeto de um mini sistema para controle dos assentos de uma sala de cinemas. Projeto referente a 1ª avaliação de Estrutura de Dados - UFPA
--- src/controle/Cliente.java
+++
@@ -1,6 +0,0 @@
-package controle;
-
-public interface Cliente {
-   // Classe interface, por natureza é uma classe abstrata e vai ser instanciada na classe Sala1_ORegresso.
-   void pegaDadosCliene();
-}
