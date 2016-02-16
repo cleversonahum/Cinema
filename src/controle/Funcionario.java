0095a2a9f05bd599ac043e1c5585b7bdcd3652a1
Jamelly <jamellyf7@gmail.com>
2016-02-16 13:30:11 -0300

    Alterações de tratamento de excessoes

--- src/controle/Funcionario.java
+++ src/controle/Funcionario.java
@@ -8,10 +8,26 @@ public class Funcionario {
     
     //Método para pedir dados do vendedor devido sistema nao possuir cadastro de funcionario.
     public void pedeDados(){
-        do{
-        this.nome = JOptionPane.showInputDialog("Vendedor, Informe seu nome: ");
-        }while(this.nome.equals("")||this.nome==null);
-       
+        
+        //vou fazer o tratamento aqui mesmo @jamelly
+        boolean sentinela = false;
+        while(sentinela == false){
+            this.nome = JOptionPane.showInputDialog("Entre com seu nome");
+            if("".equals(this.nome)){
+                sentinela =false;
+            }
+            else{
+                char[] array = this.nome.toCharArray();
+                for(int i=0;i<array.length;i++){
+                    sentinela = !Character.isDigit(array[i]);
+                }
+            }
+        }
+        
+//        do{
+//        this.nome = JOptionPane.showInputDialog("Vendedor, Informe seu nome: ");
+//        }while(this.nome.equals("")||this.nome==null);
+//       
     }
     //Métodos Get dessa classe.
     
@@ -25,5 +41,9 @@ public class Funcionario {
     }
     public void setNome(String Nome){
         this.nome = Nome;
+    }
+    public static void main(String args[]){
+        Funcionario jamelly = new Funcionario();
+        jamelly.pedeDados();
     }
 }
